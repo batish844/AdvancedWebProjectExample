8ba01a7a01699009927dec0937754c735c4894a1
Roaa Soloh <solohrk@rhu.edu.lb>
2024-10-20 11:22:23 +0300

    Add students table migration and seeder

---
+++ database/seeders/StudentsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // This import is required
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('students')->insert([
+            ['name' => 'Alice', 'email' => 'alice@example.com'],
+            ['name' => 'Bob', 'email' => 'bob@example.com'],
+        ]);
+    }
+}
